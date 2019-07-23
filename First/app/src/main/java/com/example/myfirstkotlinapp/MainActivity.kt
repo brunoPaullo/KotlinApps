0085925c7f60cf00bfd843d53f396af396113faf
brunoPaullo <brunopaullom@hotmail.com>
2019-07-22 21:49:17 -0500

    class 4

--- First/app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
+++ First/app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.myfirstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private var editName: EditText? = null
+    private var editAge: EditText? = null
+
+    private var name: String? = null
+    private var age = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editName = findViewById<EditText>(R.id.editText_name)
+        editAge = findViewById<EditText>(R.id.editText_age)
     }
 }
