efd066a0eb27e89def94f7fe175081be97899066
Bruno Paullo <bruno.paullo@cimagraf.com.pe>
2019-07-24 10:52:27 -0500

    Class 5

--- First/app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
+++ First/app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.myfirstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), TextWatcher {
     private var editName: EditText? = null
     private var editAge: EditText? = null
 
@@ -17,5 +21,18 @@ class MainActivity : AppCompatActivity() {
 
         editName = findViewById<EditText>(R.id.editText_name)
         editAge = findViewById<EditText>(R.id.editText_age)
+
+        editName?.addTextChangedListener(this)
+        editAge?.addTextChangedListener(this)
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        Toast.makeText(this, p0.toString(), Toast.LENGTH_SHORT).show()
     }
 }
