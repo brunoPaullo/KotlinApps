5c65b99b3f4a9f8f20620e004d7d6d0186b6524e
Bruno Paullo <bruno.paullo@cimagraf.com.pe>
2019-07-24 14:19:54 -0500

    Class 6

--- First/app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
+++ First/app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
@@ -5,25 +5,35 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(), TextWatcher {
     private var editName: EditText? = null
     private var editAge: EditText? = null
+    private var textName: TextView? = null
+    private var textAge: TextView? = null
 
     private var name: String? = null
-    private var age = 0
+    private var age: Int? = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editName = findViewById<EditText>(R.id.editText_name)
-        editAge = findViewById<EditText>(R.id.editText_age)
+        //editName = findViewById<EditText>(R.id.editText_name)
+        //editAge = findViewById<EditText>(R.id.editText_age)
 
-        editName?.addTextChangedListener(this)
-        editAge?.addTextChangedListener(this)
+        //textName = findViewById<TextView>(R.id.textView_Name)
+        //textAge = findViewById<TextView>(R.id.textView_Age)
+
+        //editName?.addTextChangedListener(this)
+        //editAge?.addTextChangedListener(this)
+
+        editText_name?.addTextChangedListener(this)
+        editText_age?.addTextChangedListener(this)
     }
 
     override fun afterTextChanged(p0: Editable?) {
@@ -33,6 +43,16 @@ class MainActivity : AppCompatActivity(), TextWatcher {
     }
 
     override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        Toast.makeText(this, p0.toString(), Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, p0.toString(), Toast.LENGTH_SHORT).show()
+        //On old version
+        //name = editName?.text.toString()
+        //age = Integer.valueOf(editAge?.text.toString())
+        //textName?.text = name
+        //textAge?.text = age.toString()
+
+
+        // On new version
+        textView_Name?.text = editText_name?.text.toString()
+        textView_Age?.text = editText_age?.text.toString()
     }
 }
---
+++ s
@@ -0,0 +1,35 @@
+[33mcommit b98713149bacc5dc93c7caf3a3a4c6f5d9dcc157[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: Bruno Paullo <bruno.paullo@cimagraf.com.pe>
+Date:   Wed Jul 24 11:28:40 2019 -0500
+
+    Class 6
+
+[33mcommit efd066a0eb27e89def94f7fe175081be97899066[m
+Author: Bruno Paullo <bruno.paullo@cimagraf.com.pe>
+Date:   Wed Jul 24 10:52:27 2019 -0500
+
+    Class 5
+
+[33mcommit 0085925c7f60cf00bfd843d53f396af396113faf[m
+Author: brunoPaullo <brunopaullom@hotmail.com>
+Date:   Mon Jul 22 21:49:17 2019 -0500
+
+    class 4
+
+[33mcommit 9f8a9a398436cc171048a2713c5726917cc9c08c[m
+Author: brunoPaullo <brunopaullom@hotmail.com>
+Date:   Mon Jul 22 21:14:03 2019 -0500
+
+    Add locale
+
+[33mcommit c07e57b11220dfa52c6963d0fd461a37c518a6cf[m
+Author: brunoPaullo <brunopaullom@hotmail.com>
+Date:   Mon Jul 22 20:40:14 2019 -0500
+
+    Initial source
+
+[33mcommit bc44aa8521261540d5d2740e7bc30bb73597dc7c[m
+Author: brunoPaullo <49615489+brunoPaullo@users.noreply.github.com>
+Date:   Mon Jul 22 20:26:39 2019 -0500
+
+    Initial commit
